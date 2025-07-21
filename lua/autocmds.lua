18d36cf02f73587d8d85cbe6b95d67dbc737b981
lgisler <48827180+lgisler@users.noreply.github.com>
2025-07-21 17:15:08 +0200

    Add autocmd for setting root dir

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,11 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local git_root = vim.fn.systemlist('git rev-parse --show-toplevel')[1] or ""
+    git_root = git_root:gsub("%s+$", "")
+    if git_root ~= "" and vim.fn.isdirectory(git_root) == 1 then
+      vim.o.path = git_root .. "/**"
+    end
+  end,
+})
